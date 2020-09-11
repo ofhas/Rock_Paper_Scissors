743be6749be91dbfdad61387ac65400b3af79ed7
ofhas <53121171+ofhas@users.noreply.github.com>
2020-09-11 23:15:08 +0300

    Create Rock_paper_scissors.py

---
+++ src/Rock_paper_scissors.py
@@ -0,0 +1,24 @@
+while True:
+    playerOne = input("Please enter 0 for rock 1 for paper 2 for scissors:\n")
+    playerTwo = input("Please enter 0 for rock 1 for paper 2 for scissors:\n")
+    if playerOne.isdigit() and playerTwo.isdigit():
+        playerOne = int(playerOne)
+        playerTwo = int(playerTwo)
+        if playerOne < 0 or playerOne > 2:
+            print("you've entered an invalid input for player one")
+        elif playerTwo < 0 or playerTwo > 2:
+            print("you've entered an invalid input player two")
+
+        else:
+            break
+
+
+    else:
+        print("you've entered invalid input")
+
+if (playerOne + 1) % 3 == playerTwo:
+        print("Player Two won")
+elif playerOne == playerTwo:
+    print("It is a draw")
+else:
+    print("Player One won")
